46760ce15277a120e81cb26967ad61a82d42e9a6
his7876 <his30215@naver.com>
2021-08-23 15:41:28 +0900

    기본 function 실습

---
+++ example_func.js
@@ -0,0 +1,21 @@
+let multi = function (p1, p2){
+    return p1*p2;
+}
+
+const div = (p1,p2) => {
+    return p1/p2;
+};
+
+const plus = (p1,p2) => {
+    return p1+p2;
+};
+
+
+const minus= (p1,p2) => {
+    return p1-p2;
+};
+
+console.log(multi(10,2));
+console.log(div(10,2));
+console.log(plus(10,2));
+console.log(minus(10,2));
