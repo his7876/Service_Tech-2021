733cbd801febb82d0de966258c898bac74a3775f
his7876 <his30215@naver.com>
2021-08-23 16:22:58 +0900

    구조분해 문법 + 템플릿 표현 실습

--- react_practice/example_etc.js
+++ react_practice/example_etc.js
@@ -11,17 +11,20 @@ let car = {
 //printCar(car.name,car.ph, car.sn, car.date);
 const printCar = (car) =>{
     const{name,ph,sn,date} = car;
-    console.log(name,ph,sn,date);
+    const description = "자동차의 이름은 : " +name + " 입니다. 자동차의 마력은 : " + ph+ " 입니다.";
+    const templetDescription = `자동차의 이름은 : ${name} 입니다. 자동차의 마력은 : ${ph} 입니다.`;
+    console.log(description);
+    console.log(templetDescription);
 }
 
 printCar(car);
 
-let test = {
-    name : "test",
-    age : 30,
-};
-const printTest = (test) =>{
-    const{name,age} = test;
-    console.log(name,age);
-}
-printTest(test);
+// let test = {
+//     name : "test",
+//     age : 30,
+// };
+// const printTest = (test) =>{
+//     const{name,age} = test;
+//     console.log(name,age);
+// }
+// printTest(test);
