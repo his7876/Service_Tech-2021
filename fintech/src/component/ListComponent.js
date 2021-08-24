fe2866c0690254d794a536dce847dafe414e2363
his7876 <his30215@naver.com>
2021-08-24 15:09:44 +0900

    useState 실습

--- fintech/src/component/ListComponent.js
+++ fintech/src/component/ListComponent.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
-
+import ListItem from "./ListItem";
 const ListComponent = () => {
     const [users, setUsers] = useState([
         {name : "정재현", age : 25 , height : 180},
         {name : "김정우", age : 24 , height : 182},
         {name : "김도영", age : 26 , height : 178},
     ]);
+
     return (
     <>
         <div>
             {users.map((user)=>{
-                return (<><p>{user.name} {user.age} {user.height} </p></>)
+                return (
+                <>
+                <ListItem username = {user.name}  age = {user.age} height = {user.height}></ListItem>
+                </>);
             })}
         </div>
        
