49693fa3966dadffaa50da0c3ee5c4b82f7b5978
his7876 <his30215@naver.com>
2021-08-24 14:15:55 +0900

    이벤트 연동 실습

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
-
+import EventExample from './component/EventExample';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <Header title = "메인화면"></Header>
-      <Header title = "거래 내역 조회"></Header>
+      <EventExample></EventExample>
       </header>
     </div>
   );
---
+++ fintech/src/component/EventExample.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EventExample = () => {
+    const handleChange = (e) =>{
+        console.log(e.target.value);
+    }
+    return (
+        <div>
+            <input onChange ={handleChange}></input>
+        </div>
+    );
+};
+
+export default EventExample
