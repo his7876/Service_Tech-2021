139203f47645662070571a508d7f488e4a1aac13
his7876 <his30215@naver.com>
2021-08-25 11:38:12 +0900

    news search - api

---
+++ fintech/src/component/newsSearch/NewsListItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const NewsListItem = ({title, contents, url}) => {
+    return (
+        <div>
+            <p>제목 :{title} </p>
+            <p>작성자 :{contents} </p>
+            <a href ={url}> 기사로 이동하기 </a>
+
+        </div>
+    )
+}
+
+export default NewsListItem
--- fintech/src/pages/NewsSearch.js
+++ fintech/src/pages/NewsSearch.js
@@ -1,35 +1,38 @@
-import axios from "axios";
 import React, { useState } from "react";
 import Header from "../component/Header";
 import SearchInput from "../component/newsSearch/SearchInput";
+import axios from "axios";
+import NewsList from "../component/newsSearch/NewsList";
 
 const NewsSearch = () => {
   const [serachText, setSearchText] = useState();
-  const [searchResult, setSearchResult] = useState();
+  const [searchResult, setSearchResult] = useState([]);
 
   const handleSearchTextChange = (e) => {
     const { value } = e.target;
     setSearchText(value);
   };
 
-  const handleSearchButtonClick = () =>{
+  const handleSearchButtonClick = () => {
     console.log(serachText);
     axios
-    .get(
-        `https://newsapi.org/v2/everything?q=${serachText}&from=2021-08-25&sortBy=publishedAt&apiKey=0dfeeecc456a4465995b19cd4503ff17&language=ko`
-    )
-    .then((response) => {
-      console.log(response.data.articles);
-      setSearchResult(response.data.articles);
-    });
-};
-  
+      .get(
+        `https://newsapi.org/v2/everything?q=${serachText}&from=2021-08-21&sortBy=publishedAt&apiKey=0dfeeecc456a4465995b19cd4503ff17&language=ko`
+      )
+      .then((response) => {
+        console.log(response.data.articles);
+        setSearchResult(response.data.articles);
+      });
+  };
 
   return (
     <div>
       <Header title="뉴스 검색"></Header>
-      <SearchInput handleInput={handleSearchTextChange}
-      handleClick={handleSearchButtonClick}></SearchInput>
+      <SearchInput
+        handleInput={handleSearchTextChange}
+        handleClick={handleSearchButtonClick}
+      ></SearchInput>
+      <NewsList searchResult={searchResult}></NewsList>
     </div>
   );
 };
